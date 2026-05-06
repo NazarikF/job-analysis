57c9584c2d69533e4e4862f2ba6b03401242b4e0
NazarikF <https://github.com/NazarikF>
2026-05-06 20:30:48 +0300

    load and format job data output

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-data = [1000, 120, 200, 300, 400, 300, 200, 100]
-import matplotlib.pyplot as plt
+import json
 
-days = (range(1, len(data) + 1))
+with open('../data/jobs.json') as f:
+    data = json.load(f)
+    for i, job in enumerate(data,1):
+        print(f'{i}',job['title'],"\n", job['company'],'\n', job['description'])
 
-plt.plot(days, data)
-plt.title('Price for days')
-plt.xlabel('Days')
-plt.ylabel('Price')
-plt.show()
-print(min(data), max(data), max(data) / 2)
+
+
